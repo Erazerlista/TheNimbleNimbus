390434cd09ad5d6fc9923dcc957c131cd64ad4a2
Erazerlista <128334161+Erazerlista@users.noreply.github.com>
2023-06-26 15:27:48 -0700

    added java code

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,47 @@
-let searchButton = document.getElementById('search-button');
-let weatherInput = document.querySelector('#weather-search');
-let repoContainer = document.querySelector('#repo-container');
-let repoSearchTerm = document.querySelector('#repo-search-term');
-let apiKey = "d68a2ac9d30df41f9d6ca649fb111b00";
-let ident = "...";
+const apiKey = 'd68a2ac9d30df41f9d6ca649fb111b00';
 
-function getApi(searchTerm) {
-    var recipeURL = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}"+apiKey+"&app_id="+ident+"&q="+searchTerm+"&type=public";
-    fetch(recipeURL).then(function (response) {
-        if (response.ok) {
-          response.json().then(function (data) {
-            console.log(data.hits);
-            displayRecipes(data.hits);
-            saveRecipes(data.hits);
-          });
-        } else {
-          alert('Error: ' + response.statusText);
-        }
+document.addEventListener('DOMContentLoaded', () => {
+  const searchButton = document.getElementById('search-button');
+
+  searchButton.addEventListener('click', () => {
+    const searchInput = document.getElementById('city-search');
+    const city = searchInput.value;
+
+    if (city.trim() !== '') {
+      getWeatherData(city);
+      searchInput.value = '';
+    }
+  });
+});
+
+function getWeatherData(city) {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      // Process the response data
+      console.log(data);
+      displayForecast(data.list); // Pass the forecast array to the display function
+    })
+    .catch(error => {
+      // Handle any errors that occurred during the API request
+      console.log('An error occurred:', error);
     });
 }
 
-var formSubmitHandler = function (event) {
-    event.preventDefault();
-    var ingrSearch = recipeInput.value.trim();
-    if (ingrSearch) {
-      getApi(ingrSearch);
-      recipeInput.value = '';
-    } else {
-      alert('Please enter a city to search with');
-    }
-};
+function displayForecast(forecastArray) {
+  const forecastContainer = document.getElementById('forecast-container');
 
+  // Clear previous forecast data
+  forecastContainer.innerHTML = '';
+
+  // Loop through the forecast array
+  forecastArray.forEach(forecast => {
+    const forecastItem = document.createElement('div');
+    forecastItem.textContent = forecast.weather[0].description; // Display the weather description
+
+    // Add the forecast item to the container
+    forecastContainer.appendChild(forecastItem);
+  });
+}
