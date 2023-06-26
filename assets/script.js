5762b340a09883319295c32f94319edfb470ffb8
Erazerlista <128334161+Erazerlista@users.noreply.github.com>
2023-06-26 14:04:33 -0700

    added logo and more code

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,9 @@
-
+let searchButton = document.getElementById('search-button');
+let weatherInput = document.querySelector('#weather-search');
+let repoContainer = document.querySelector('#repo-container');
+let repoSearchTerm = document.querySelector('#repo-search-term');
+let apiKey = "d68a2ac9d30df41f9d6ca649fb111b00";
+let ident = "...";
 
 function getApi(searchTerm) {
     var recipeURL = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}"+apiKey+"&app_id="+ident+"&q="+searchTerm+"&type=public";
@@ -14,3 +19,15 @@ function getApi(searchTerm) {
         }
     });
 }
+
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+    var ingrSearch = recipeInput.value.trim();
+    if (ingrSearch) {
+      getApi(ingrSearch);
+      recipeInput.value = '';
+    } else {
+      alert('Please enter a city to search with');
+    }
+};
+
